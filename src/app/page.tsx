326767ce7ad877c2a6665cd236a4fb8d3e008718
Emilio Andere <andere@uchicago.edu>
2024-04-13 12:06:26 -0500

    design stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+'use client';
+
 import { PodcastPlayer } from '@/components/podcast-player'
 import { SearchBar } from '@/components/search-bar'
+import { useState } from 'react'
 
 export default function Home() {
+  const [processed, setProcessed] = useState(false)
   return (
     <>
      <main className="flex min-h-screen flex-col items-center justify-center space-y-10">
       <div className="w-[500px] justify-center font-mono text-sm lg:flex">
         <SearchBar />
       </div>
-       <PodcastPlayer />
+       {processed && <PodcastPlayer />}
       <Footer /> 
     </main>
     </>
@@ -17,21 +21,16 @@ export default function Home() {
 
 function Footer() {
   return (
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols lg:text-center font-mono">
+      <div className="mt-20 mb-12 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols lg:text-center font-mono">
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/technoabsurdist/research-pods"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-1 text-lg font-semibold`}>
-            See the code{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
           <p className={`m-0 text-xs opacity-50`}>
-            Star Github repository to support the project.
+            <span className='underline'><strong>Github</strong></span>.{' '}
+            By @technoabsurdist
           </p>
         </a>
      </div>
--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -2,11 +2,16 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export function SearchBar() {
+  
+  const submitLink = () => {
+    console.log('submitting link')
+  }
+
   return (
     <div className="flex w-full items-center space-x-4">
       <div className="flex w-full max-w-lg items-center space-x-4">
         <Input aria-describedby="file-description" placeholder="Enter arxiv link to convert" type="text" />
-        <Button type="submit">Convert</Button>
+        <Button onClick={submitLink} type="submit">Convert</Button>
       </div>
     </div>
   )
